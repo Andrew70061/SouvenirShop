5c638acad8b30d460d7d95db29bd6f41f5db6e8e
Andrew70061 <and70061@gmail.com>
2024-08-25 01:41:53 +0300

    25.08.2024

--- shop/views.py
+++ shop/views.py
@@ -48,10 +48,6 @@ def search(request):
 
 
 #Категории товаров
-from django.shortcuts import render, get_object_or_404
-from django.db.models import Q, Min, Max
-from .models import Product, ProductCategory
-
 def category_detail(request, slug):
     category = get_object_or_404(ProductCategory, slug=slug)
     products = Product.objects.filter(category=category, publish=True)
