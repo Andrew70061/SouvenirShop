2100f9d4162b9537a1a99ae307ce74ab0a7d2f66
Andrew70061 <and70061@gmail.com>
2024-09-22 23:46:32 +0300

    22.09.2024 Создании логики для подсчета заказов за месяц и вывода гистограммы на начальную страницу

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,7 +1,12 @@
 from .models import ProductCategory
 from django import template
 from .models import Order
+from django.db.models import Count
 from django.utils import timezone
+from datetime import timedelta
+import io
+import base64
+import matplotlib.pyplot as plt
 
 
 #Категории
@@ -16,15 +21,40 @@ def get_item(dictionary, key):
     return dictionary.get(key)
 
 
-#Статистика по заказам на начальной странице на действующую дату
+#Статистика по заказам на начальной странице: на действующую дату и за 30 дней в гистограмме
 def order_stats(request):
     today = timezone.now().date()
     new_orders_count = Order.objects.filter(status=Order.STATUS_NEW, created__date=today).count()
     cancelled_orders_count = Order.objects.filter(status=Order.STATUS_CANCELLED, created__date=today).count()
     completed_orders_count = Order.objects.filter(status=Order.STATUS_COMPLETED, created__date=today).count()
 
+    # Гистограмма заказов за месяц
+    end_date = timezone.now()
+    start_date = end_date - timedelta(days=30)
+    orders_per_day = Order.objects.filter(created__range=(start_date, end_date)).values('created__date').annotate(count=Count('id'))
+
+    dates = [order['created__date'] for order in orders_per_day]
+    counts = [order['count'] for order in orders_per_day]
+
+    plt.figure(figsize=(10, 5))
+    plt.bar(dates, counts, color='blue')
+    plt.xlabel('Дата')
+    plt.ylabel('Количество заказов')
+
+    # Форматирование меток оси X
+    plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%d'))
+    plt.gca().xaxis.set_major_locator(plt.matplotlib.dates.DayLocator())
+    plt.xticks(rotation=0)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+
     return {
         'new_orders_count': new_orders_count,
         'cancelled_orders_count': cancelled_orders_count,
         'completed_orders_count': completed_orders_count,
+        'orders_chart': image_base64,
     }
